21ae397a495509171b11560a3cfae48b2ae07401
pranavz <pranavzinzurde@gmail.com>
2023-08-15 01:57:46 +0530

    user auth

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,18 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from"./config/dbconnect.js"
 import cors from "cors"
+import userRoutes from "./routes/authRoute.js"
 
 dotenv.config()
 
 connectDb()
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+
 app.use(cors())
 app.use(express.json())
+app.use('/api/users', userRoutes)
 
 app.get("/", (req, res)=> {
   res.send('API is running...')
@@ -18,4 +21,4 @@ app.get("/", (req, res)=> {
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-});
+})
