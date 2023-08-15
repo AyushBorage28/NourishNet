3343ce13d4123905e78559db3a9827710b8dc880
Ishpreet-Kaur-Bedi <ishpreetshriya@gmail.com>
2023-08-15 15:58:08 +0530

    Framer motion added to landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,50 +1,81 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { heroImage } from "../assets";
+import { FaArrowRight, FaPhoneAlt } from "react-icons/fa";
 
 const Hero = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1 } },
+  };
+
+  const textVariants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1, delay: 0.5 } },
+  };
+  const rotatingImageStyles = {
+    width: "100%",
+    height: "auto",
+    borderRadius: "8px",
+    transform: "rotate(360deg)",
+    animation: "rotation 20s linear infinite",
+  };
+
+
   return (
-    <section className="bg-white dark:bg-gray-900">
-      <div className="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="place-self-center mr-auto lg:col-span-7">
-          <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Joining Hands, Nourishing Hearts
+    <motion.section
+      className="bg-white dark:bg-gray-900"
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
+    >
+      <div className="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-3 lg:grid-cols-12">
+        <motion.div
+          className="place-self-center lg:col-span-6 p-6 md:p-10 lg:p-12"
+          variants={textVariants}
+        >
+          <h1 className="text-4xl font-extrabold leading-tight md:text-5xl xl:text-6xl text-gray-800 dark:text-white">
+            Transforming Excess into Equity
           </h1>
-          <p className="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          <h3 className="mt-4 text-xl font-semibold text-gray-600 dark:text-gray-300">
+            Nourishing a Better Tomorrow
+          </h3>
+          <p className="mt-6 text-lg text-gray-600 dark:text-gray-400">
             Bridging the HoReCa Industry and NGOs for Instant Food Rescue and
-            Community Nourishment. Transform Food Waste into Hopeful Meals with
-            NourishNet.
+            Community Nourishment. Join us and transform food waste into
+            hopeful meals with NourishNet.
           </p>
-          <a
-            href="/signup"
-            className="inline-flex justify-center items-center py-3 px-5 mr-3 text-base font-medium text-center text-white rounded-lg bg-[#eda277] hover:bg-[#de7e44] focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-          >
-            Join us
-            <svg
-              className="ml-2 -mr-1 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+          <div className="mt-8 space-x-4">
+            <a
+              href="/signup"
+              className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-[#EDA277] rounded-lg hover:bg-[#DE7E44] transition-colors"
             >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
-          <a
-            href="#"
-            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-          >
-            Speak to Sales
-          </a>
-        </div>
-        <div className="hidden lg:mt-0  lg:col-span-5 lg:flex">
-          <img src={heroImage} alt="mockup" className="w-30 h-30" />
-        </div>
+              Join us <FaArrowRight className="ml-2" />
+            </a>
+            <a
+              href="#"
+              className="inline-flex items-center px-6 py-3 text-base font-medium text-gray-700 border border-gray-300 rounded-lg hover:bg-gray-200 transition-colors"
+            >
+              Speak to Sales <FaPhoneAlt className="ml-2" />
+            </a>
+          </div>
+        </motion.div>
+        <motion.div
+          className="lg:mt-0 lg:col-span-6 p-6"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+               <motion.img
+            src={heroImage}
+            alt="mockup"
+            style={rotatingImageStyles}
+          />
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
 export default Hero;
+
