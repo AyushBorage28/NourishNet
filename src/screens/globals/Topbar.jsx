ca3cb144513e0f15a98007274d2a5fec9346d733
Ishpreet-Kaur-Bedi <ishpreetshriya@gmail.com>
2023-08-17 04:26:49 +0530

    DropDown added

--- src/screens/globals/Topbar.jsx
+++ src/screens/globals/Topbar.jsx
@@ -1,5 +1,7 @@
-import { Box, IconButton, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import { Box, IconButton, useTheme, Menu, MenuItem } from "@mui/material";
 import { useContext } from "react";
+import { Link } from "react-router-dom"; // Make sure this import is correct
 import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
@@ -7,26 +9,32 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
+import Dashboard from "../dashboard"; // Check if this path is correct
+import Home from "../Home"; // Check if this path is correct
 
 const Topbar = () => {
-  const theme = useTheme(); // Bring mui theme
-  const colors = tokens(theme.palette.mode); //Bring dark/light tokens and add it to mui theme
-  const colorMode = useContext(ColorModeContext); //
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
-      <Box
-        display="flex"
-        backgroundColor={colors.primary[400]}
-        borderRadius="3px"
-      >
+      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
 
         <IconButton type="button" sx={{ p: 1 }}>
-          <SearchIcon />
+    
         </IconButton>
-        
       </Box>
 
       <Box display="flex">
@@ -43,9 +51,29 @@ const Topbar = () => {
         <IconButton>
           <SettingsOutlinedIcon />
         </IconButton>
-        <IconButton>
+        <IconButton onClick={handleMenuOpen}>
           <PersonOutlinedIcon />
         </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem onClick={handleMenuClose}>
+            <Link to="#">Profile</Link>
+          </MenuItem>
+          <MenuItem onClick={handleMenuClose}>
+            <Link to="/">Logout</Link>
+          </MenuItem>
+        </Menu>
       </Box>
     </Box>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import reactRouterDom from 'react-router-dom';
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
