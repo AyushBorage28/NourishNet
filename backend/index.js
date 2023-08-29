63b54ae70f44125f30ef6c4409dff5d1adcba080
pranavz <pranavzinzurde@gmail.com>
2023-08-30 01:41:59 +0530

    ping backend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDb from"./config/dbconnect.js"
 import cors from "cors"
 import userRoutes from "./routes/authRoute.js"
+import schedule from "node-schedule"
 
 dotenv.config()
 
@@ -14,6 +15,14 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 app.use('/api/users', userRoutes)
+
+// make api request to '/' every 5 minutes to keep this app awake
+schedule.scheduleJob('*/5 * * * *', function(){
+  fetch('https://nourishnet-backend-k736.onrender.com')
+  .then(res => res.json({message: "pinged"}))
+  .then(json => console.log(json))
+  .catch(err => console.log(err))
+})
 
 app.get("/", (req, res)=> {
   res.send('API is running...')
